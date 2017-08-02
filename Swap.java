ac9f00bf90d579ad58358224c5faf475256fce76
himmat lodhi <singhhimmat256@gmail.com>
2017-08-01 23:43:12 -0700

    swapping program

---
+++ Swap.java
@@ -0,0 +1,13 @@
+class Swap
+{
+  public static void main(String... s)
+  {
+    int a=3,b=4;
+  System.out.println("Before Swaping"+a+"  "+b);
+   a=a+b;
+   b=a-b;
+   a=a-b;
+ System.out.println("After Swaping"+a+"  "+b);
+ 
+  }
+}

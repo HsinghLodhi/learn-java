e53ea2cc6076bfd7d9a3a81865ebdfe3aeb71c91
himmat lodhi <singhhimmat256@gmail.com>
2017-08-04 05:13:23 -0700

    hello word

---
+++ First.cpp
@@ -0,0 +1,12 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+ {
+ 	int a,b,c;
+ 	clrscr()
+ 	printf("Enter two numbers");
+ 	scanf("%d %d",&a,&b);
+ 	c=a+b;
+ 	printf("sum is %d",c);
+ 	getch();
+	 }

969855c26899982cb6a9ae4639292af1fab1f62b
himmat lodhi <singhhimmat256@gmail.com>
2017-08-27 17:37:33 -0700

    Arithmetic operater

---
+++ Arithmetic.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int maun()
+{
+	int a=17,b=4;
+	printf("sum=%d\n",a+b);
+	printf("defference=%d\n",a-b);
+	printf("products=%d\n",a*b);
+	printf("quotient=%d\n",a/b);
+	printf("remainder=%d\n,a%b");
+	getch();
+}

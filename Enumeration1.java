af516914ef358587238251902122f972e17fb25c
himmat lodhi <singhhimmat256@gmail.com>
2017-09-03 08:07:29 -0700

    number

---
+++ Enumeration1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Enumeration1
+ {
+ public static void main(String... args)
+ Vector v = new Vector();
+ For(int i=0; i<=10; i++);
+  {
+   v.addElement(i);
+  }
+  System.out.println(v);
+
+ }
